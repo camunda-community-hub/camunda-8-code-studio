b813c1c9cabd9c9c0f6b97cbb2dc871ad3dad3ee
Thomas <thomas.heinrichs@camunda.com>
2022-04-07 09:43:24 +0200

    Added SpringBoot Client

--- src/JavaCloudWorker/src/main/java/zeebe/camunda/worker/WorkerApplication.java
+++ src/JavaCloudWorker/src/main/java/zeebe/camunda/worker/WorkerApplication.java
@@ -6,6 +6,7 @@ import io.camunda.zeebe.client.api.worker.JobClient;
 import io.camunda.zeebe.client.impl.oauth.OAuthCredentialsProvider;
 import io.camunda.zeebe.client.impl.oauth.OAuthCredentialsProviderBuilder;
 import io.camunda.zeebe.spring.client.EnableZeebeClient;
+import io.camunda.zeebe.spring.client.annotation.ZeebeVariable;
 import io.camunda.zeebe.spring.client.annotation.ZeebeWorker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -13,22 +14,73 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 @SpringBootApplication
 @EnableZeebeClient
 @RestController
 public class WorkerApplication {
+	static Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+	static ConsoleHandler handler = new ConsoleHandler();
+
 	public static void main(String[] args) {
 		SpringApplication.run(WorkerApplication.class, args);
+		logger.setLevel(Level.ALL);
+		handler.setLevel(Level.ALL);
+		logger.addHandler(handler);
 	}
 
-	@ZeebeWorker(type = "recipe-worker")
-	public void handleJobFoo(final JobClient client, final ActivatedJob job) {
+	@ZeebeWorker(type = "determine_quarantine_period")
+	public void determineQuarantinePeriod(final JobClient client, final ActivatedJob job, @ZeebeVariable String person_uuid, @ZeebeVariable String quarantine_duration) {
+		logger.info("Retrieving metadata for person "+ person_uuid +" from external database...");
+		logger.info("Processing Business Rules to determine quarantine duration...");
+
+		if(quarantine_duration.equals(null)) {
+			client.newCompleteCommand(job.getKey())
+					.variables("{\"quarantine_duration\": \"P10D\"}")
+					.send()
+					.exceptionally(throwable -> {
+						throw new RuntimeException("Could not complete job " + job, throwable);
+					});
+			logger.info("Set quarantine_duration to 10 Days");
+		}
+
+		logger.info("Set quarantine_duration to " + quarantine_duration);
+		client.newCompleteCommand(job.getKey())
+				.variables("")
+				.send()
+				.exceptionally(throwable -> {
+					throw new RuntimeException("Could not complete job " + job, throwable);
+				});
+	}
+
+	@ZeebeWorker(type = "notify_person_to_quarantine", autoComplete = true)
+	public void notifyPersonToQuarantine(final JobClient client, final ActivatedJob job, @ZeebeVariable String person_uuid) {
+		logger.info("Retrieving contact details for person " + person_uuid + " from external database...");
+		logger.info("Sending notification to person " + person_uuid + " to quarantine...");
+	}
+
+	@ZeebeWorker(type = "generate_certificate_of_recovery")
+	public void generateCertificateOfRecovery(final JobClient client, final ActivatedJob job, @ZeebeVariable String person_uuid) {
+		UUID recovery_certificate_uuid = UUID.randomUUID();
+		logger.info("Generating certificate of recovery for person " + person_uuid +"...");
+		logger.info("Generated certificate ID: " + recovery_certificate_uuid);
+		logger.info("Storing Recovery Certificate in external database...");
 
 		client.newCompleteCommand(job.getKey())
-				.variables("...")
+				.variables("{\"recovery_certificate_uuid\": \"" + recovery_certificate_uuid + "\"}")
 				.send()
 				.exceptionally( throwable -> { throw new RuntimeException("Could not complete job " + job, throwable); });
 	}
 
+	@ZeebeWorker(type = "send_certificate_of_recovery", autoComplete = true)
+	public void sendCertificateOfRecovery(final JobClient client, final ActivatedJob job, @ZeebeVariable String person_uuid, @ZeebeVariable String recovery_certificate_uuid) {
+		logger.info("Retrieving Recovery Certificate " + recovery_certificate_uuid + " from external database...");
+		logger.info("Retrieving contact details for person " + person_uuid + "from external database...");
+		logger.info("Sending Recovery Certificate to person " + person_uuid + ". Enjoy that ice-cream!");
+	}
 }
